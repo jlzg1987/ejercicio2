4edab1a658626a789de389089e37704908c19ba3
jlzg1987 <jlzg1987@hotmail.com>
2023-02-22 23:13:25 -0500

    update

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -8,7 +8,7 @@ router.get('/',(req,res)=>{
 
 router.post('/',  (req, res) => {
 const  body =req.body;
-console.log (res.status(200).json({body}))
+res.status(200).json(body)
 });
 
 module.exports=router;
